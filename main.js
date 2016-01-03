4572e8e3879339e3e288d7bf2ac413e1a501012d
Cathy A. Miller <catmiller07@comcast.net>
2016-01-02 20:18:49 -0500

    new commit

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 $(document).ready(function (){
-      $(".contact-modal").toggle(); 
+    //  $(".contact-modal").toggle(); 
     
-      //show contact form
-      $("#contact-link").click(function (){
-             $(".contact-modal").toggle(); 
-      });
+     
 });
